6fefcc9283fff354c5e363fb4a3bc70e3aed97f7
IA1719 <his.lhk.1@gmail.com>
2025-11-25 13:53:49 +0900

    commit

---
+++ src/test/java/com/demo/home/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
