cf2b5893d572cf45302cd7abf2f8f766262dd4d4
IA1719 <his.lhk.1@gmail.com>
2025-11-25 17:42:47 +0900

    DB연결확인

--- src/test/java/com/demo/home/SpringBootBoardApplicationTests.java
+++ src/test/java/com/demo/home/SpringBootBoardApplicationTests.java
@@ -2,12 +2,15 @@ package com.demo.home;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class SpringBootBoardApplicationTests {
-
+	
 	@Test
-	void contextLoads() {
+	void databaseConnection() {
+		
+		
 	}
 
 }
